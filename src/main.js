f746b3df99f9af2bae566ee3b4dfba8babcc81db
Ann-tech-del <annodhiambo87@gmail.com>
2025-05-29 12:42:10 +0300

    modified the app
    added id to the submit button
    removed the border that i placed for styling purposes

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 let photoInput = document.querySelector("#form-name-input-2");
 let factInput = document.querySelector("#form-name-input");
-const imgBtn = document.querySelectorAll(".search-btn")[1];
-const factBtn = document.querySelectorAll(".search-btn")[0];
+const imgBtn = document.querySelector("#searchBtn2");
+const factBtn = document.querySelector("#searchBtn1");
 const result = document.querySelector(".result");
-
 
 async function getCatPhotos() {
   result.innerHTML = "";
@@ -34,7 +33,6 @@ async function getCatPhotos() {
   }
 }
 
-
 async function getCatFacts() {
   result.innerHTML = "";
 
@@ -59,7 +57,6 @@ async function getCatFacts() {
     result.textContent = "Failed to load cat facts.";
   }
 }
-
 
 imgBtn.addEventListener("click", function (e) {
   e.preventDefault();
